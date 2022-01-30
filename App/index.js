57126b9b1831d722e681aab974d4ca27949a04db
ins4w <amejia.foo@gmail.com>
2022-01-30 13:21:24 -0500

    add worked time property to period object and fixed work end date issues

--- App/index.js
+++ App/index.js
@@ -1,7 +1,5 @@
 let scrapButton = document.getElementById("scrap-btn");
 
-console.log("hola en el contexte de la extension");
-
 scrapButton.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   if (tab !== null) {
@@ -13,16 +11,65 @@ scrapButton.addEventListener("click", async () => {
 });
 
 const profileScrapper = () => {
-  const queries = {
-    name: document.querySelector(
-      "#ember36 > div.ph5.pb5 > div.mt2.relative > div:nth-child(1) > div:nth-child(1) > h1"
-    ),
+  const querySelectorForEach = (array, selector) =>
+    array.map((item) => item.querySelector(selector));
+
+  let nombre = document.querySelector(
+    "div.ph5 > div.mt2.relative > div:nth-child(1) > div:nth-child(1) > h1"
+  );
+
+  let experienciasLaborales = Array.from(
+    document.querySelectorAll(
+      "#ember144 > div.pvs-list__outer-container > ul > li"
+    )
+  );
+
+  let roles = querySelectorForEach(
+    experienciasLaborales,
+    "div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between > div.display-flex.flex-column.full-width > div > span > span:nth-child(1)"
+  );
+
+  let funciones = querySelectorForEach(
+    experienciasLaborales,
+    "div > div.display-flex.flex-column.full-width.align-self-center > div.pvs-list__outer-container > ul > li > div > ul > li > div > div > div > div > span.visually-hidden"
+  );
+
+  let empresas = querySelectorForEach(
+    experienciasLaborales,
+    "div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between > div.display-flex.flex-column.full-width > span:nth-child(2) > span:nth-child(1)"
+  );
+
+  let periodos = querySelectorForEach(
+    experienciasLaborales,
+    "div > div.display-flex.flex-column.full-width.align-self-center > div.display-flex.flex-row.justify-space-between > div.display-flex.flex-column.full-width > span.t-14.t-normal.t-black--light > span:nth-child(1)"
+  );
+
+  const informacionExtraida = {
+    Nombre: nombre.innerText,
+    Trabajos: [],
   };
 
-  const linkedinProfile = {
-    name: queries.name?.innerText,
-  };
+  for (let i = 0; i < experienciasLaborales.length; i++) {
+    let periodo = periodos[i].innerText.split("·");
+    let rangoPeriodo = periodo[0].split("-");
+    let fechaFinal =
+      rangoPeriodo.length === 1
+        ? rangoPeriodo[0].trim()
+        : rangoPeriodo[1].trim();
+    informacionExtraida["Trabajos"] = [
+      ...informacionExtraida["Trabajos"],
+      {
+        Empresa: empresas[i]?.innerText,
+        Rol: roles[i]?.innerText,
+        Periodo: {
+          Inicio: rangoPeriodo[0].trim(),
+          Final: fechaFinal,
+          Tiempo: periodo[1].trim(),
+        },
+        Funciones: funciones[i]?.innerText,
+      },
+    ];
+  }
 
-  console.log(linkedinProfile);
-  console.log("hola en pagina a inyectar");
+  console.log(informacionExtraida);
 };
